8a9ff6e6d67d5e2aa8c772281547b25c207a7472
CahTarine <camille.tarine@gmail.com>
2024-11-23 17:04:26 -0300

    deploy do projeto

--- server.js
+++ server.js
@@ -1,10 +1,19 @@
+// Importa o módulo Express, que é a base para criar aplicações web Node.js.
 import express from "express";
+import routes from "./src/routes/postRouts.js";
 
+// Cria uma instância do Express, que será o objeto principal da nossa aplicação.
 const app = express();
+app.use(express.static("uploads"))
+routes(app)
+
+// Habilita o middleware (intermediário entre duas coisas, ex: aplicativo e banco de dados) para converter o corpo das requisições em formato JSON (que é uma forma simples de organizar os daods para as máquinas entenderem). 
+app.use(express.json());
+
+// Inicia o servidor Express na porta 3000 e exibe uma mensagem no console quando o servidor estiver pronto.
 app.listen(3000, () => {
     console.log("Servidor escutando...");
 });
 
-app.get("/api", (req, res) => {
-    res.status(200).send("Boas vindas a imerssão.");
-});
+
+
